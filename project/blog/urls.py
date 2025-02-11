ebdee331684d97b9f00733fbe2f3dc39a2cb31be
vasundhra2002 <vasundhra2002@gmai.com>
2025-02-11 19:54:15 +0530

    firstcomm

---
+++ project/blog/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.PostListViews.as_view(), name='post_list'),
+    url(r'^about/$',views.AboutView.as_view(), name='about'),
+    url(r'post/(?P<pk>\d+)$',views.PostDetailView.as_views(),name='post_detail'),
+    url(r'post/new/$',views.CreatePostView(),name='post_new'),
+    url(r'post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_views(),name='post_edit'),
+    url(r'post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_views(),name='post_remove'),
+    url(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+    url(r'post/(?P<pk>\d+)/comment/$',views.add_comment_to_post,name='add_comment_to_post'),
+    url(r'comment/(?P<pk>\d+)/approve/$',views.comment_approve,name='comment_approve'),
+    url(r'comment/(?P<pk>\d+)/remove/$',views.comment_approve,name='comment_remove'),
+    url(r'post/(?P<pk>\d+)/publish/$',views.post_publish,name='post_publish'),
+]
+
+
+
